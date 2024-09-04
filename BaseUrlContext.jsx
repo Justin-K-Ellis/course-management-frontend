e5fc9a870095c473862d5b7685dfc7ced2d91bcf
Justin Klitgaard <jlke45@gmail.com>
2024-09-04 21:19:36 +0900

    Upate API base url.

--- BaseUrlContext.jsx
+++ BaseUrlContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const BaseUrlContext = createContext("http://localhost:3000");
+const BaseUrlContext = createContext(import.meta.env.VITE_API);
 
 export { BaseUrlContext };
