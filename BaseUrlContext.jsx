a8040bf3eaca16e0bd39e7afd9a441f1fc7a63df
Justin Klitgaard <jlke45@gmail.com>
2024-09-04 22:33:46 +0900

    Update base url

--- BaseUrlContext.jsx
+++ BaseUrlContext.jsx
@@ -1,5 +1,8 @@
 import { createContext } from "react";
 
-const BaseUrlContext = createContext(import.meta.env.VITE_API);
+// const BaseUrlContext = createContext(import.meta.env.VITE_API);
+const BaseUrlContext = createContext(
+  "https://course-management-1p45.onrender.com"
+);
 
 export { BaseUrlContext };
