ccb7211c26d492edf334130f2b2ba39963d5a40c
Justin Klitgaard <jlke45@gmail.com>
2024-09-05 18:43:56 +0900

    Update loading spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,15 @@
-import { Grid } from "react-loader-spinner";
+// import { Grid } from "react-loader-spinner";
 
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-full text-accent">
-      <Grid
+      <div className="flex w-52 flex-col gap-4">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>
+      {/* <Grid
         visible={true}
         height="80"
         width="80"
@@ -12,7 +18,7 @@ const LoadingSpinner = () => {
         radius="12.5"
         wrapperStyle={{}}
         wrapperClass="grid-wrapper"
-      />
+      /> */}
     </div>
   );
 };
